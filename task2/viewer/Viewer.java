8a9ed154b6e58e19b4115fefa88543737440171e
Kate-Sav <67168836+Kate-Sav@users.noreply.github.com>
2020-09-30 01:24:47 +0300

    Update Viewer.java

--- task2/viewer/Viewer.java
+++ task2/viewer/Viewer.java
@@ -5,9 +5,8 @@ import by.htp.task2.entity.Library;
 
 public class Viewer {
 	public void printAuthor(Library library) {
-		System.out.println("-------------");
 		System.out.println("Author - " + library.getBook(0).getAuthor());
-		System.out.println("-------------");
+		System.out.println();
 		for(Book b : library.getBooks()) {
 			System.out.println(b.getTitle());
 		}
